31cd408bef90c5bdbfa4cb4c6ac675d8c7f07b5e
Gleb Stolypinsky <stolypinskii@gmail.com>
2021-08-22 17:53:40 +0300

    Все объявления на главной

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
-from webapp.model import db
-from webapp.flat.read_json import get_metro
+from webapp.model import db, Flat
 from flask_migrate import Migrate
 
 
@@ -10,13 +9,8 @@ def create_app():
     db.init_app(app)
     migrate = Migrate(app, db)
 
-    @app.route('/search')
-    def search():
-        list_metro = get_metro()
-        num_rooms = [0, 1, 2, 3, 4, 5, 6, 7]
-        return render_template('search.html', num_rooms=num_rooms, list_metro=list_metro)
-
-    @app.route('/result', methods=['POST'])
-    def result():
-        return render_template('no_result.html')
-    return app
+    @app.route('/index')
+    def index():
+        flats = Flat.query.all()
+        num_offer = len(flats)
+        return render_template('index.html', flats=flats, num_offer=num_offer)
