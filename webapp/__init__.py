6f28b74bb17cd3217ad8f6cd14964c04090406d8
Gleb Stolypinsky <stolypinskii@gmail.com>
2021-08-30 21:21:33 +0300

    Начальная фильтрация

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,8 @@
-from operator import and_
 from flask import Flask, render_template, request
 from webapp.model import db, Flat
 from flask_migrate import Migrate
 from sqlalchemy import and_
+
 
 def create_app():
     app = Flask(__name__)
@@ -13,10 +13,8 @@ def create_app():
     @app.route('/index')
     def index():
         flats = Flat.query.all()
-        num_offer = len(flats)
-        #return render_template('index.html', flats=flats, num_offer=num_offer)
-        return render_template('ex.html', flats=flats)
-    
+        return render_template('index.html', flats=flats)
+
     @app.route('/rep', methods=["POST", "GET"])
     def rep():
         num_rooms = [0, 1, 2, 3, 4, 5]
@@ -24,12 +22,11 @@ def create_app():
         if request.method == 'POST':
             if request.form.getlist('num_rooms'):
                 num_rooms = [int(i) for i in request.form.getlist('num_rooms')]
-                print(num_rooms)
             if request.form.get('from'):
                 price_from = request.form.get('from')
             else:
                 price_from = 0
-            
+
             if request.form.get('to'):
                 price_to = request.form.get('to')
             else:
@@ -39,20 +36,20 @@ def create_app():
                 commission = 0
             else:
                 commission = 100
-            
+
             if request.form.get('deposit'):
                 deposit = 0
             else:
                 deposit = 1000000
-            
+
             if request.form.getlist('material'):
                 material = request.form.getlist('material')
-            
+
             if request.form.get('min'):
                 area_min = request.form.get('min')
             else:
                 area_min = 0
-            
+
             if request.form.get('max'):
                 area_max = request.form.get('max')
             else:
@@ -62,8 +59,8 @@ def create_app():
                                       Flat.commission <= commission,
                                       Flat.deposit <= deposit,
                                       and_(Flat.area <= area_max, Flat.area >= area_min)).all()
-            return render_template('ex.html', flats=query)
+
+            return render_template('index.html', flats=query)
         query = Flat.query.all()
-        print(request.method)
-        return render_template('ex.html', flats=query)
+        return render_template('index.html', flats=query)
     return app
