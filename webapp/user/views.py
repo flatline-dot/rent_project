065dcda6abbfb08414fb424ecb4411c8d3914481
Gleb Stolypinsky <stolypinskii@gmail.com>
2021-09-12 19:18:57 +0300

    Регистрация пользователей

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user
-from webapp.user.form import LoginForm
-from webapp.model import User
+from webapp.user.form import LoginForm, RegistrationForm
+from webapp.model import db, User
 
 
 blueprint = Blueprint('user', __name__)
@@ -23,6 +23,7 @@ def process_login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             flash('Вы вошли на сайт')
+            print(current_user)
             return redirect(url_for('ads.index'))
     flash('Неверное имя пользователя или пароль')
     return redirect(url_for('user.login'))
@@ -32,3 +33,25 @@ def process_login():
 def logout():
     logout_user()
     return redirect(url_for('ads.index'))
+
+
+@blueprint.route('/registration')
+def registation():
+    if current_user.is_authenticated:
+        return redirect(url_for('ads.index'))
+    form = RegistrationForm()
+    return render_template('user/registration.html', form=form)
+
+
+@blueprint.route('/process-regisration', methods=["POST"])
+def process_registration():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, email=form.email.data, role='user')
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Вы успешно зарегестрировались')
+        return redirect(url_for('user.login'))
+    flash('Пожалуйста исправьте ошибки в форме')
+    return redirect(url_for('user.registration'))
